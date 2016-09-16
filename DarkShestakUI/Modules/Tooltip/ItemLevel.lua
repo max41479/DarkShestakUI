d6ec9475ff1f9f7c852eec9438ce53a1c0e533b5
max41479 <cod41479@list.ru>
2016-09-16 11:37:31 +0300

    Fixed typo.

--- DarkShestakUI/Modules/Tooltip/ItemLevel.lua
+++ DarkShestakUI/Modules/Tooltip/ItemLevel.lua
@@ -187,7 +187,7 @@ local function UnitGear(unit)
                             local numBonusIDs = tonumber(strmatch(itemLink, ".+:%d+:512:%d*:(%d+).+"))
                             if numBonusIDs then
                                 if GetDetailedItemLevelInfo then
-                                    local effectiveLevel, previewLevel, origLevel = GetDetailedItemLevelInfo(clink)
+                                    local effectiveLevel, previewLevel, origLevel = GetDetailedItemLevelInfo(itemLink)
                                     level = effectiveLevel or level
                                 end
                             end
